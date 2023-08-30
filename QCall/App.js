06ddba175a40974c24212955a90da6150776b750
A2ITNotes <muadh117a@gmail.com>
2023-08-30 19:05:56 +0100

    starting boilerplate

--- QCall/App.js
+++ QCall/App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Welcome from './components/Welcome';
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName='Welcome'
+        screenOptions={{ headerStyle: { backgroundColor: '#FBDABB' } }}>  
+              <Stack.Screen name='Welcome' component={Welcome} />
+        </Stack.Navigator> 
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
