ac0bebe52239a7e29050f8716a845cfae877754b
A2ITNotes <muadh117a@gmail.com>
2023-09-04 18:06:32 +0100

    raw location data from sensor gotten

--- QCall/App.js
+++ QCall/App.js
@@ -1,20 +1,54 @@
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Welcome from './components/Welcome';
-const Stack = createStackNavigator();
+import React, { useState, useEffect } from "react";
+import { Platform, Text, View, StyleSheet } from "react-native";
+import * as Device from "expo-device";
+import * as Location from "expo-location";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator 
-        initialRouteName='Welcome'
-        screenOptions={{ headerStyle: { backgroundColor: '#FBDABB' } }}>  
-              <Stack.Screen name='Welcome' component={Welcome} />
-        </Stack.Navigator> 
-    </NavigationContainer>
-  );
+	const [location, setLocation] = useState(null);
+	const [errorMsg, setErrorMsg] = useState(null);
+
+	useEffect(() => {
+		(async () => {
+			if (Platform.OS === "android" && !Device.isDevice) {
+				setErrorMsg(
+					"Oops, this will not work on Snack in an Android Emulator. Try it on your device!"
+				);
+				return;
+			}
+			let { status } = await Location.requestForegroundPermissionsAsync();
+			if (status !== "granted") {
+				setErrorMsg("Permission to access location was denied");
+				return;
+			}
+
+			let location = await Location.getCurrentPositionAsync({});
+			setLocation(location);
+		})();
+	}, []);
+
+	let text = "Waiting...";
+	if (errorMsg) {
+		text = errorMsg;
+	} else if (location) {
+		text = JSON.stringify(location);
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.paragraph}>{text}</Text>
+		</View>
+	);
 }
 
-
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		padding: 20,
+	},
+	paragraph: {
+		fontSize: 18,
+		textAlign: "center",
+	},
+});
