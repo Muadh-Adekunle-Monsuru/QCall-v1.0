9747c497cbb7c40eca4bd4ac33d5ac751db41953
A2ITNotes <muadh117a@gmail.com>
2023-09-06 14:33:00 +0100

    Update NumberSection.js

--- QCall/components/NumberSection.js
+++ QCall/components/NumberSection.js
@@ -6,12 +6,35 @@ import {
 	StyleSheet,
 	Pressable,
 	Alert,
+	Button,
 } from "react-native";
 
 export default function NumberSection() {
+	const [responseData, setResponseData] = useState(null);
+
+	const fetchData = async () => {
+		try {
+			const response = await fetch(
+				"https://sheetdb.io/api/v1/sc073hiofw97m/search?lganame=oron&sheet=data"
+			);
+			const data = await response.json();
+			setResponseData(data);
+		} catch (error) {
+			console.error("Error fetching data:", error);
+		}
+	};
+
 	return (
 		<View style={styles.container}>
 			<Text style={styles.blockHeading}>Contacts</Text>
+
+			<Button title="Fetch Data" onPress={fetchData} />
+			{responseData && (
+				<View>
+					<Text>API Response:</Text>
+					<Text>{JSON.stringify(responseData, null, 2)}</Text>
+				</View>
+			)}
 		</View>
 	);
 }
